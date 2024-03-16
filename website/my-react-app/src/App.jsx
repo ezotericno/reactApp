13a0d09f98f3d54b2125a92f8da24dd7d5a1fecd
Anthony P <65885798+ezotericno@users.noreply.github.com>
2024-03-16 09:22:56 +0000

    made external folder for button component

--- website/my-react-app/src/App.jsx
+++ website/my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
-import Button from './Button.jsx'
+import Button from './Button/Button.jsx'
 
 function App() {
   
