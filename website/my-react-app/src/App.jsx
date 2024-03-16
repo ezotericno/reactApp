78a6cfe4a36c9f39f3df6c04366cea438295514c
Anthony P <65885798+ezotericno@users.noreply.github.com>
2024-03-16 09:43:21 +0000

    added student component

--- website/my-react-app/src/App.jsx
+++ website/my-react-app/src/App.jsx
@@ -2,10 +2,15 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
 import Button from './Button/Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
   
-  return(<Button></Button>);
+  return(
+    <>
+    <Student name="Spongebob"/>
+    </>
+  );
 }
 
 export default App
