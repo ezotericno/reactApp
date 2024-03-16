d2fbdc03cc1a69eb2177ba23c7cfdc5b189dd7c0
Anthony P <65885798+ezotericno@users.noreply.github.com>
2024-03-16 09:08:59 +0000

    created card component

--- website/my-react-app/src/App.jsx
+++ website/my-react-app/src/App.jsx
@@ -1,13 +1,13 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import Card from './Card.jsx'
 
 function App() {
   
   return(
-    <>
-    <Header></Header>
-    <Footer></Footer>
-    </>
+    
+    <Card></Card>
+    
   );
 }
 
---
+++ website/my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-img" src="https://avatars.githubusercontent.com/u/65885798?v=4" alt="profile picture"></img>
+            <h2 className="card-title">Anthony Pritchard</h2>
+            <p className="card-text">Asspiring CyberSecurity professional and IOT programmer</p>
+        </div>
+    );
+}
+
+export default Card
