f210b04855233823c515f55078a9a61df1e7849c
Anthony P <65885798+ezotericno@users.noreply.github.com>
2024-03-16 09:15:14 +0000

    created button class

--- website/my-react-app/src/App.jsx
+++ website/my-react-app/src/App.jsx
@@ -1,14 +1,11 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
+import Button from './Button.jsx'
 
 function App() {
   
-  return(
-    
-    <Card></Card>
-    
-  );
+  return(<Button></Button>);
 }
 
 export default App
